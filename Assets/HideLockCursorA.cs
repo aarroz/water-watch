55391055f3711a5fc5ec55318337550e7b87a04c
BuildTools <unconfigured@null.spigotmc.org>
2016-07-28 18:21:47 -0700

    initial commit

---
+++ Assets/HideLockCursorA.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideLockCursorA : MonoBehaviour
+{
+	void Start ()
+	{ Cursor.lockState = CursorLockMode.Locked; Cursor.visible = false;
+	
+	} 
+
+	void Update () { Cursor.lockState = CursorLockMode.Locked; Cursor.visible = false;
+		if (Input.GetKey("escape"))
+			Application.Quit();
+	}
+
+
+}
--- README.md
+++
@@ -1,5 +0,0 @@
-# water-watch
-
-WIP Game By AArroz
-
-For progress check the updates.
